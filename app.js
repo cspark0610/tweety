860ac0c85c635fd63809364f201d89d89225fbf5
cspark0610 <cpark0610@gmail.com>
2021-01-20 19:46:33 -0300

    term

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
-const express = require( 'express' );
-const morgan = require('morgan'); //middleware application logger
-const nunjucks = require( 'nunjucks' );
+//const { request } = require("express");
+const express = require("express");
+const morgan = require("morgan"); //middleware application logger
+const nunjucks = require("nunjucks");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 const app = express(); // crea una instancia de una aplicación de express
 
+// let tweetsDeEjemplo = [
+// 	{ id: 1, name: "juan", content: "este es un tweeettt de juan" },
+// 	{ id: 2, name: "carlos", content: "este es un tweeettt de carlos" },
+// 	{ id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
+// ];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function logueo(req, res, next) {
+	console.log(req.params);
+
+	let user = req.params.name;
+	if (user === "pepe" || user === "carlos" || user === "juan") {
+		next();
+	} else {
+		res.sendStatus(403);
+	}
+}
+
 // Configurando Nunjucks
-app.set('view engine', 'html'); // hace que res.render funcione con archivos html
-app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
-nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
+app.set("view engine", "html"); // hace que res.render funcione con archivos html
+app.engine("html", nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure("views"); // apunta a nunjucks al directorio correcto para los templates
 
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
+app.use('/',routes);
 
-let tweetsDeEjemplo = [
-    { id: 1, name: "juan", content: "este es un tweeettt de juan" },
-    { id: 2, name: "carlos", content: "este es un tweeettt de carlos" },
-    { id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
-];
 
-app.get('/', function (req, res) {
-    res.render( 'index', { tweets: tweetsDeEjemplo });
+// app.get("/:id/:name", logueo, function (req, res) {
+
+// 	res.render("index", { tweets: tweetsDeEjemplo });
+// });
+
+app.listen(3000, function () {
+	console.log("Estas escuhando en el puerto 3000");
 });
-
-app.listen(3000, function(){
-    console.log('Estas escuhando en el puerto 3000')
-});
-
-
-
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,44 @@
 // escribir rutas aquí
+const express = require('express');
+const router = express.Router();
+
+const tweetBank = require('../tweetBank');
+
+//agrego los metodos get
+router.get('/', function (req, res) {
+    console.log(req.body);
+  let tweets = tweetBank.list();
+  res.render( 'index',{ tweets: tweets, showForm: true } );
+});
+
+
+
+router.get('/users/:name', function(req, res) {
+ 
+    let name = req.params.name;
+    let tweets = tweetBank.find( { name: name } );
+    res.render( 'index', { tweets: tweets, showForm: true, twitero: name} );
+  });
+//el 2do parametro de render es un objeto, con dist propiedades
+
+router.get('/tweets/:id', function(req, res) {
+ 
+    let id = parseInt(req.params.id);
+    let tweets = tweetBank.find( { id: id } );
+    res.render( 'index', { tweets: tweets} );
+  });
+
+router.post('/' ,(req,res)=>{
+    let name = req.body.name;
+    let text = req.body.text;
+    
+    tweetBank.add({ name: name, text: text });
+
+    //bajo ECMA6 puedo hacer add({name, text})
+    res.sendStatus(201);
+
+});
+
+
+
+module.exports = router;
