d20d06c06ab2c372db5813e7bd67a701ac872c30
cspark0610 <cpark0610@gmail.com>
2021-01-21 14:49:27 -0300

    mod 21/1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //const { request } = require("express");
+
 const express = require("express");
 const morgan = require("morgan"); //middleware application logger
 const nunjucks = require("nunjucks");
 const bodyParser = require("body-parser");
-const routes = require("./routes");
+const routes = require("./routes"); //importo todo lo contenido en la 
+//carpeta routes
 
 const app = express(); // crea una instancia de una aplicación de express
 
@@ -13,18 +15,20 @@ const app = express(); // crea una instancia de una aplicación de express
 // 	{ id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
 // ];
 
+//ARMO LA LOGICA DE LO Q LA APP DEBERIA HACER SECUENCIALMENTE
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// function logueo(req, res, next) {
+// 	console.log(req.params);
 
-function logueo(req, res, next) {
-	console.log(req.params);
-
-	let user = req.params.name;
-	if (user === "pepe" || user === "carlos" || user === "juan") {
-		next();
-	} else {
-		res.sendStatus(403);
-	}
-}
+// 	let user = req.params.name;
+// 	if (user === "pepe" || user === "carlos" || user === "juan") {
+// 		next();
+// 	} else {
+// 		res.sendStatus(403);
+// 	}
+// }
 
 // Configurando Nunjucks
 app.set("view engine", "html"); // hace que res.render funcione con archivos html
@@ -32,14 +36,15 @@ app.engine("html", nunjucks.render); // cuando le den archivos html a res.render
 nunjucks.configure("views"); // apunta a nunjucks al directorio correcto para los templates
 
 app.use(morgan("tiny"));
-app.use('/',routes);
+app.use(routes);//1
 
 
 // app.get("/:id/:name", logueo, function (req, res) {
-
 // 	res.render("index", { tweets: tweetsDeEjemplo });
 // });
 
 app.listen(3000, function () {
 	console.log("Estas escuhando en el puerto 3000");
 });
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,33 @@
 // escribir rutas aquí
 const express = require('express');
 const router = express.Router();
-
 const tweetBank = require('../tweetBank');
+module.exports = router;
+
+
+// const server = require('../app.js')
+// const socketIo = require('socket.io');
+// const io = socketIo(server)
+// io.on('connection', ()=>{
+//   console.log('new connection');
+// });
+
+// module.exports = function (io){
+//   return router
+// }
+
 
 //agrego los metodos get
 router.get('/', function (req, res) {
-    console.log(req.body);
   let tweets = tweetBank.list();
   res.render( 'index',{ tweets: tweets, showForm: true } );
 });
 
 
-
 router.get('/users/:name', function(req, res) {
- 
+    console.log(req.body);
     let name = req.params.name;
-    let tweets = tweetBank.find( { name: name } );
+    let tweets = tweetBank.find( { "name": name } );
     res.render( 'index', { tweets: tweets, showForm: true, twitero: name} );
   });
 //el 2do parametro de render es un objeto, con dist propiedades
@@ -24,21 +35,22 @@ router.get('/users/:name', function(req, res) {
 router.get('/tweets/:id', function(req, res) {
  
     let id = parseInt(req.params.id);
-    let tweets = tweetBank.find( { id: id } );
+    let tweets = tweetBank.find( { "id": id } );
     res.render( 'index', { tweets: tweets} );
   });
 
-router.post('/' ,(req,res)=>{
+router.post('/tweets' ,(req,res)=>{
     let name = req.body.name;
-    let text = req.body.text;
+    let content = req.body.content;
     
-    tweetBank.add({ name: name, text: text });
-
-    //bajo ECMA6 puedo hacer add({name, text})
-    res.sendStatus(201);
+    tweetBank.add(name , content);
+    console.log(tweetBank);
+    res.send("tweet agregado!!!")
+    //res.send({name, content});
+    
+    //res.sendStatus(201);
 
 });
 
 
 
-module.exports = router;
